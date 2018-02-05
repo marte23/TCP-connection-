b48e2b1d7d323f92f4c35b3376884b04a7495ee8
Informatica <Informatica@localhost>
2018-02-05 09:37:52 +0100

    Programma Ultimato

---
+++ src/Connection/ServerConnessioneTCP.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import static java.lang.System.out;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Francesco Martellotti
+ */
+public class ServerConnessioneTCP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // porta del server maggiore di 1024 
+        int port=2000;
+        //oggetto ServerSocket necessario per accettare richieste dal client
+        ServerSocket sSocket = null;
+        //oggetto da usare per realizzare la connessione TCP
+        Socket connection;
+        DataInputStream in = null;
+        DataOutputStream out = null;
+        while(true){
+            try{
+                // il server si mette in ascolto sulla porta voluta
+                sSocket = new ServerSocket(port);
+                System.out.println("In attesa di connessioni!");
+                //si è stabilita la connessione
+                connection = sSocket.accept();
+                System.out.println("Connessione stabilita!");
+                System.out.println("Socket server: " + connection.getLocalSocketAddress());
+                System.out.println("Socket client: " + connection.getRemoteSocketAddress());
+                in = new DataInputStream(connection.getInputStream());
+                System.out.println("Richiesta client "+ in.readUTF());
+                out= new DataOutputStream(connection.getOutputStream());
+
+                SimpleDateFormat sdf = new SimpleDateFormat(); // creo l'oggetto
+                String dataStr = sdf.format(new Date()); // data corrente
+
+                sdf.applyPattern("dd/MM/yyyy");  
+                
+                System.out.println(dataStr);
+                out.flush();
+            }
+               catch(IOException e){
+                   System.err.println("Errore di I/O!");
+            }
+            
+            //chiusura della connessione con il client
+            try {
+                if (sSocket!=null) sSocket.close();
+            } catch (IOException ex) {
+                System.err.println("Errore nella chiusura della connessione!");
+            }
+            System.out.println("Connessione chiusa!");
+        }
+      }
+}
